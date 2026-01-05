d7b381e05866a61a1d7b9cec3c05e593bf74d612
Adinath Shinde <adinathshinde8010851905@gmail.com>
2026-01-05 16:24:17 +0530

    Change home route response message in Flask app

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "GitHub Actions CI/CD Pipeline Working!"
+    return "Hello from Flask CI/CD!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
